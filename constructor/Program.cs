390b2cd98b4dbcda970eeec0cb68f08d8f721cf6
ROY <ROY@DESKTOP-26H23PD>
2022-02-20 18:12:56 +0530

    Add project files.

---
+++ constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace hello
+{
+    class MyApp
+    {
+        public string name;
+        public int a;
+        public MyApp(string name)
+        {
+            this.name = name;
+
+        }
+        public void get(int a)
+        {
+            this.a = a;
+            Console.WriteLine(this.name);
+            Console.WriteLine(this.a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyApp obj = new MyApp("Mona");
+            obj.get(10);
+
+        }
+    }
+}
